a1d1349def250354696acab4a0182308ea113cce
orangedrake <petr.fiala@atlas.cz>
2024-06-02 09:10:55 +0200

    Phase 4 alter the entity and the table

--- src/main/java/petrfiala/task/entity/User.java
+++ src/main/java/petrfiala/task/entity/User.java
@@ -23,4 +23,15 @@ public class User {
     @Column(name = "name", nullable = false)
     private String name;
 
+    @NotBlank(message = "Username is mandatory")
+    @Size(min = 3, max = 50, message = "Username must be between 3 and 50 characters")
+    @Pattern(regexp = "^[A-Za-z0-9]+$", message = "Username must be alphanumeric and cannot contain special characters")
+    @Column(name = "username", nullable = false, unique = true)
+    private String username;
+
+    @NotBlank(message = "Password is mandatory")
+    @Size(min = 8, max = 255, message = "Password must be between 8 and 255 characters")
+    @Column(name = "password", nullable = false)
+    private String password;
+
 }
--- src/main/resources/schme.sql
+++ src/main/resources/schme.sql
@@ -8,3 +8,17 @@ INSERT INTO users (name) VALUES
 ('Jane Doe'),
 ('Alice Smith'),
 ('Bob Johnson');
+
+
+ALTER TABLE users
+ADD COLUMN username VARCHAR(127),
+ADD COLUMN password VARCHAR(255);
+
+UPDATE users
+SET username = regexp_replace(name, '\s', '', 'g'),
+    password = substring(md5(random()::text) from 1 for 12);
+
+ALTER TABLE users
+ALTER COLUMN username SET NOT NULL,
+ALTER COLUMN password SET NOT NULL,
+ADD CONSTRAINT unique_username UNIQUE (username);
