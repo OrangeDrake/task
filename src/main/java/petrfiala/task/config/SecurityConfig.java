fce922d9519d4807a3881bafee44797e160320af
orangedrake <petr.fiala@atlas.cz>
2024-06-03 11:07:41 +0200

    Phase 4 inmemory users together with database users

--- src/main/java/petrfiala/task/config/SecurityConfig.java
+++ src/main/java/petrfiala/task/config/SecurityConfig.java
@@ -1,7 +1,6 @@
 package petrfiala.task.config;
 
-import petrfiala.task.service.CustomUserDetailsService;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -10,6 +9,8 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import petrfiala.task.service.CustomUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Configuration
 @EnableWebSecurity
@@ -17,6 +18,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private CustomUserDetailsService userDetailsService;
+
+    @Value("${spring.security.user.name}")
+    private String inMemoryUsername;
+
+    @Value("${spring.security.user.password}")
+    private String inMemoryPassword;
+
+    @Value("${spring.security.user.roles}")
+    private String inMemoryRoles;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -34,7 +44,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
         auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+
+        auth.inMemoryAuthentication()
+                .withUser(inMemoryUsername)
+                .password(passwordEncoder().encode(inMemoryPassword))
+                .roles(inMemoryRoles.split(","));
     }
 
     @Bean
